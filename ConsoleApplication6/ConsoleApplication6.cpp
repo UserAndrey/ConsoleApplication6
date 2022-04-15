bb896317c4b452cc4ff64fa241c6d15cfcf682d3
User <User@WIN-A4TOTEJDR9Q>
2022-04-15 14:11:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+int main(void)
+{
+	double result;
+	double PI_2 = asin(1);
+	const double PI = 3.141592653589793;
+	float i;
+	/* printf("Input angle:",i);
+	 scanf("%d",i);   */
+	for (i = 0.00000001; i <= 10; i = i + 0.00001)
+	{
+		result = cos(PI*i);
+		printf("The cosine of %f *PI is %f \n", i, result);
+	}
+	_getch();
+	return 0;
+}
+
